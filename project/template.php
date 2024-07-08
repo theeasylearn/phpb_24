7e592b332b25e1b41af5c2d4474b898118771a2e
theeasylearn <theeasylearn@gmail.com>
2024-07-08 21:10:36 +0530

    template

---
+++ project/template.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div id="menu">
+        <ul>
+            <li><a href="#">Sample Link</a></li>
+            <li><a href="#">Sample Link</a></li>
+            <li><a href="#">Sample Link</a></li>
+            <li><a href="#">Sample Link</a></li>
+            <li><a href="#">Sample Link</a></li>
+            <li><a href="#">Sample Link</a></li>
+            <li><a href="#">Sample Link</a></li>
+        </ul>
+    </div>
+    <div id="heading">
+        <h2>Module Heading</h2>
+    </div>
+    <table id="data">
+        <thead>
+            <tr>
+                <th>Heading</th>
+                <th>Heading</th>
+                <th>Heading</th>
+                <th>Heading</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Content</td>
+                <td>Content</td>
+                <td>Content</td>
+                <td>Content</td>
+            </tr>
+            
+        </tbody>
+    </table>
+</body>
+
+</html>
