9341d032063f12e1aa3870b3c99cad60ff2e1872
theeasylearn <theeasylearn@gmail.com>
2024-07-23 21:17:17 +0530

    datatables

---
+++ project/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lenovo/project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '56c0b851a5ba2e6ca2d7c55f78bcb13c4f86aa70',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'components/jquery' => array(
+            'pretty_version' => 'v3.7.1',
+            'version' => '3.7.1.0',
+            'reference' => '8edc7785239bb8c2ad2b83302b856a1d61de60e7',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/jquery',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'lenovo/project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '56c0b851a5ba2e6ca2d7c55f78bcb13c4f86aa70',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
