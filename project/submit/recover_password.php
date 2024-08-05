cc9446d8f502fec950b8336d79a4b4846b496d61
theeasylearn <theeasylearn@gmail.com>
2024-08-05 21:10:17 +0530

    email sending

--- project/submit/recover_password.php
+++ project/submit/recover_password.php
@@ -23,10 +23,21 @@ try
         // $_SESSION['success'] = "login successfully";
         // $_SESSION['id'] = $table[0]['id'];
         // header("location:../lecture.php");
-        //generate randome password 
         require_once("../inc/common_functions.php");
+
+        //generate randome password 
         $newpassword = generatePassword(8);
-        echo $newpassword;
+        $sql = 'update admin set password=? where email=?';
+        $cmd = $db->prepare($sql);
+        $data = array($newpassword,$email);
+        //query execute
+        $cmd->execute($data);
+        require_once("../inc/mail_function.php");
+        $subject = "Password recovery email";
+        $message = "your new password $newpassword";
+        SendMail($email,$subject,$message);
+        $_SESSION['success'] = "password sent on email";
+        header("location:../index.php");
     }
 } catch (PDOException $error) {
     LogError($error);
